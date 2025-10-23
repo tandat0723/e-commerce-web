e3627d25bcd303e8cee8a15a330f3dcc4c91f061
Tan Dat <trantandat0723@gmail.com>
2025-10-23 20:38:31 +0700

    feat: use mongodb and update control user panel

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+const { unique } = require("next/dist/build/utils");
+
+const userSchema = new mongoose.Schema({
+    _id: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    imageUrl: { type: String, require: true },
+    cartItems: { type: Object, default: {} },
+}, { minimize: false })
+
+const User = mongoose.model.user || mongoose.model('user', userSchema)
+
+export default User
